ca0ce7f875496428829e7c6a873fea38deb84e3a
TanLiangYeu <liangyeu2994@gmail.com>
2024-09-03 22:46:49 +0800

    Update DomesticFlight.java

--- DomesticFlight.java
+++ DomesticFlight.java
@@ -1,7 +1,30 @@
+/**
+ * @author simji
+ * This show the blueprint of Flight Booking System
+ */
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * class Domestic Flight
+ */
 public class DomesticFlight extends Flight {
+
+    /**
+     *
+     * @param flightCode
+     * String flightCode
+     * @param departure
+     * String departure
+     * @param destination
+     * String destination
+     * @param price
+     * double price
+     * @param flightDate
+     * LocalDate flightDate
+     * @param departTime
+     * LocalTime departTime
+     */
     public DomesticFlight(String flightCode, String departure, String destination, double price, LocalDate flightDate, LocalTime departTime) {
         super(flightCode, departure, destination, price, flightDate, departTime);
     }
