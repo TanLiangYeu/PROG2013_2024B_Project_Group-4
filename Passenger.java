21a16758bbaa971e574ffa3a1ad402bc2bd85704
TanLiangYeu <liangyeu2994@gmail.com>
2024-09-03 22:53:11 +0800

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,14 +1,30 @@
+/**
+ * class Passenger
+ */
 public class Passenger {
     private String name;
     private String icNumber;
     private String phoneNumber;
 
+    /**
+     *
+     * @param name
+     * String name
+     * @param icNumber
+     * String icNumber
+     * @param phoneNumber
+     * String phoneNumber
+     */
     public Passenger(String name, String icNumber, String phoneNumber) {
         this.name = name;
         this.icNumber = icNumber;
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     *
+     * @return Name, IC, Phone Number
+     */
     public String getPassengerInfo() {
         return "Name: " + name + "\nIC/Passport No: " + icNumber + "\nPhone Number: " + phoneNumber;
     }
