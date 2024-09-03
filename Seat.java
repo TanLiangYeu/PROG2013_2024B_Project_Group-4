fc3a7cc781326e3be534541acc6d8c07bbf4392f
TanLiangYeu <liangyeu2994@gmail.com>
2024-09-03 22:50:02 +0800

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,10 +1,22 @@
+/**
+ * class seat
+ */
 public class Seat {
     private String seatNumber;
 
+    /**
+     *
+     * @param seatNumber
+     * String seatNumber
+     */
     public Seat(String seatNumber) {
         this.seatNumber = seatNumber;
     }
 
+    /**
+     *
+     * @return get seat info
+     */
     public String getSeatInfo() {
         return "Seat: " + seatNumber;
     }
