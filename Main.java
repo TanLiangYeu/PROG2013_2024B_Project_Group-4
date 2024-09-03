7fb137e0a642eebaf8dedfb5c16160b077e0167d
TanLiangYeu <liangyeu2994@gmail.com>
2024-09-03 22:45:04 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+/**
+ * @author simji
+ * This show the blueprint of Flight Booking System
+ */
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * This show the blueprint of flight Booking System
+ * @author simji
+ */
 public class Main extends Application {
+    /**
+     * @param primaryStage
+     * Flight Booking System
+     */
     @Override
     public void start(Stage primaryStage) {
         FlightTypeSelectionPage flightTypeSelectionPage = new FlightTypeSelectionPage(primaryStage);
@@ -9,7 +21,9 @@ public class Main extends Application {
         primaryStage.setTitle("Flight Booking System");
         primaryStage.show();
     }
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
