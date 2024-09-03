6ea7e19690f65f5fdacc3417aa4d26d4a68b4afc
TanLiangYeu <liangyeu2994@gmail.com>
2024-09-03 22:55:16 +0800

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,26 +1,30 @@
+/**
+ * class Booking
+ */
 public class Booking {
     private Flight flight;
     private Passenger passenger;
     private Seat seat;
-     /**
-         *
-         * @param flight 
-         * Flight flight
-         * @param passenger
-         * Passenger passenger
-         * @param seat
-         * Seat seat
-         */
 
+    /**
+     *
+     * @param flight 
+     * Flight flight
+     * @param passenger
+     * Passenger passenger
+     * @param seat
+     * Seat seat
+     */
     public Booking(Flight flight, Passenger passenger, Seat seat) {
         this.flight = flight;
         this.passenger = passenger;
         this.seat = seat;
     }
-        /**
-         *
-         * @return Booking Info
-         */
+
+    /**
+     *
+     * @return Booking Info
+     */
     public String getBookingInfo() {
         return "Booking Successful!\n\n-Flight Information-\n" + flight.getFlightInfo() + "\n\n-Selected Seat-\n" + seat.getSeatInfo() + "\n\n-Passenger Information-\n" + passenger.getPassengerInfo();
     }
